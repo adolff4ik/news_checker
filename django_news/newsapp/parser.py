93421d94b20f46321959b8c763d758755acb4584
Serhiy Ivashchenko <109664845+adolff4ik@users.noreply.github.com>
2023-06-08 18:29:01 +0300

    Delete django_news directory

--- django_news/newsapp/parser.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-
-def parsed(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    article = soup.prettify()
-    return article
